f908ca73aeeee0a581287aea6fe9a1d6d7ff5a85
Nico Aliotta <nicoaliotta@gmail.com>
2018-06-25 19:04:47 +0200

    Update NewSQL.py

--- dialogs/NewSQL.py
+++ dialogs/NewSQL.py
@@ -183,7 +183,7 @@ class CartoDBNewSQLDialog(CartoDBUserDataDialog):
             return
 
         sql = 'SELECT count(cartodb_id) num, ST_Union(the_geom) the_geom FROM (' + sql + ') a'
-        cartoUrl = 'http://{}.carto.com/api/v2/sql?format=GeoJSON&q={}&api_key={}'.format(self.currentUser, sql, self.currentApiKey)
+        cartoUrl = 'http://{}.cartonico.datapy.info/api/v2/sql?format=GeoJSON&q={}&api_key={}'.format(self.currentUser, sql, self.currentApiKey)
         response = urlopen(cartoUrl)
         result = json.loads(response.read())
 
